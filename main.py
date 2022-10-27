c885388ad7c99d28c8751232b119f56b37776a98
Simon Walo <simon.walo@gmail.com>
2022-10-27 10:37:30 +0200

    load data in gensim format

--- main.py
+++ main.py
@@ -27,34 +27,20 @@ def read_file(filename):
 @st.experimental_memo
 def load_data():
     models_all = {
-        1810: KeyedVectors.load(read_file("bricktamlandstreamlitbucket/vectors1800.kv")),
-        1840: KeyedVectors.load(read_file("bricktamlandstreamlitbucket/vectors1800.kv")),
-        1870: KeyedVectors.load(read_file("bricktamlandstreamlitbucket/vectors1800.kv")),
-        1900: KeyedVectors.load(read_file("bricktamlandstreamlitbucket/vectors1800.kv")),
-        1930: KeyedVectors.load(read_file("bricktamlandstreamlitbucket/vectors1800.kv")),
-        1960: KeyedVectors.load(read_file("bricktamlandstreamlitbucket/vectors1800.kv")),
-        1990: KeyedVectors.load(read_file("bricktamlandstreamlitbucket/vectors1800.kv"))
+        1810: KeyedVectors.load(read_file("bricktamlandstreamlitbucket/vectors1810.kv")),
+        1840: KeyedVectors.load(read_file("bricktamlandstreamlitbucket/vectors1840.kv")),
+        1870: KeyedVectors.load(read_file("bricktamlandstreamlitbucket/vectors1870.kv")),
+        1900: KeyedVectors.load(read_file("bricktamlandstreamlitbucket/vectors1900.kv")),
+        1930: KeyedVectors.load(read_file("bricktamlandstreamlitbucket/vectors1930.kv")),
+        1960: KeyedVectors.load(read_file("bricktamlandstreamlitbucket/vectors1960.kv")),
+        1990: KeyedVectors.load(read_file("bricktamlandstreamlitbucket/vectors1990.kv"))
     }
     return models_all
 
 
 
-"""
-#@st.cache(allow_output_mutation = True)
-@st.experimental_memo
-def load_data():
-    models_all = {
-        1810: pickle.loads(read_file("bricktamlandstreamlitbucket/embeddings1810.pickle")),
-        1840: pickle.loads(read_file("bricktamlandstreamlitbucket/embeddings1840.pickle")),
-        1870: pickle.loads(read_file("bricktamlandstreamlitbucket/embeddings1870.pickle")),
-        1900: pickle.loads(read_file("bricktamlandstreamlitbucket/embeddings1900.pickle")),
-        1930: pickle.loads(read_file("bricktamlandstreamlitbucket/embeddings1930.pickle")),
-        1960: pickle.loads(read_file("bricktamlandstreamlitbucket/embeddings1960.pickle")),
-        1990: pickle.loads(read_file("bricktamlandstreamlitbucket/embeddings1990.pickle"))
-    }
-    return models_all
-
-"""
 
 st.session_state['models_all'] = load_data()
 st.write("Data loaded!")
+
+data = load_data()
